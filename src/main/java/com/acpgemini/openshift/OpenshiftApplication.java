c7701fc9e9187db2293e0c8070d9906d4dc0366a
skolapar <skolapar@LIN65000371.corp.capgemini.com>
2019-09-18 22:49:24 +0530

    hjhj

--- src/main/java/com/acpgemini/openshift/OpenshiftApplication.java
+++ src/main/java/com/acpgemini/openshift/OpenshiftApplication.java
@@ -2,10 +2,15 @@ package com.acpgemini.openshift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class OpenshiftApplication {
 
+	@RequestMapping("/hello")  
+    public String hello(){  
+        return"Hello!";  
+    } 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenshiftApplication.class, args);
 	}
